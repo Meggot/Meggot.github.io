e0ea24f240a3fda4ccb0a7ef844dbd6934c9384c
Meggot <bradleyw@artifexnet.co.uk>
2016-09-28 01:32:31 +0100

    Commited all code.

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+var path = __dirname + '/views/';
+
+//Get BODYPARSER for POST requests.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/index",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/portfolio",function(req,res){
+  res.sendFile(path + "portfolio.html");
+});
+
+router.get("/blog",function(req,res){
+  res.sendFile(path + "blog.html");
+});
+
+router.get("/static/styles.css",function(req,res) {
+    res.sendFile(__dirname + "/static/styles.css");
+});
+
+router.get("/images/cthulu.png", function(req,res) {
+    res.sendFile(path+ "/images/cthulu.png");
+});
+
+router.get("/api", function(req,res) {
+    res.json({ message: 'Hi, i spy with my API..'
+    });
+});
+    
+app.use("/",router);
+
+app.use('/api', router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
